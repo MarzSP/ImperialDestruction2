9435adb49e5cb25f9a32f0e4ffcb8e659f732486
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-11-06 21:32:41 +0100

    refactor

--- src/main/java/AlienDestruction/Scenes/EndScreen.java
+++ src/main/java/AlienDestruction/Scenes/EndScreen.java
@@ -24,58 +24,51 @@ public class EndScreen extends MenuManager {
 
     private TextEntity finalScoreText;
 
-
-    /**
-     * Constructor: initialiseert de app referentie met de meegegeven App object. Dit zodat er een sceneverandering kan plaatsvinden.
-     * @param app
-     *
-     * Ook initialiseert het de player referentie met het meegegeven Player object. Dit zodat de score kan worden opgehaald uit Player.
-     */
     public EndScreen(App app) {
         this.app = app;
 
     }
 
-
-    /**
-     * setupScene zet de achtergrondafbeelding van deze scene.
-     */
     @Override
     public void setupScene() {
         setBackgroundImage("backgrounds/universe1.jpg");
     }
 
-    /**
-     * De methode setupEntities initialiseert en voegt de entiteiten toe aan de scene.
-     * New TextEntity: Game Over
-     * New TextEntity: finalScoreText: haalt uit Player de score op, op het moment dat de levens van de player op zijn
-     * PlayAgainButton entiteit, welke een eigen class is: een actie om het spel opnieuw te starten.
-     * QuitGameButton entiteit, welke een eigen class is: een actie om het spel te beeindigen.
-     */
     @Override
     public void setupEntities() {
+        addGameOverText();
+        addFinalScoreText();
+        addPlayAgainButton();
+        addQuitGameButton();
+    }
+
+    private void addGameOverText() {
         var gameMenuText = new TextEntity(
                 new Coordinate2D(getWidth() / 2, getHeight() / 4),
                 "GAME OVER!");
         gameMenuText.setAnchorPoint(AnchorPoint.TOP_CENTER);
         gameMenuText.setFill(Color.GOLD);
-        gameMenuText.setFont(Font.font("Roboto", FontWeight.BOLD, getWidth()/25));
+        gameMenuText.setFont(Font.font("Roboto", FontWeight.BOLD, getWidth() / 25));
         addEntity(gameMenuText);
+    }
 
+    private void addFinalScoreText() {
         finalScoreText = new TextEntity(
                 new Coordinate2D(getWidth() / 2, getHeight() / 3),
                 "Score: " + Player.getFinalScore());
         finalScoreText.setAnchorPoint(AnchorPoint.TOP_CENTER);
         finalScoreText.setFill(Color.ANTIQUEWHITE);
-        finalScoreText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, getHeight()/ 20));
+        finalScoreText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, getHeight() / 20));
         addEntity(finalScoreText);
+    }
 
-        PlayAgainButton playButton;
-        playButton = new PlayAgainButton(new Coordinate2D(getWidth() / 2, getHeight() * 4/6), app);
+    private void addPlayAgainButton() {
+        var playButton = new PlayAgainButton(new Coordinate2D(getWidth() / 2, getHeight() * 4 / 6), app);
         addEntity(playButton);
+    }
 
-       var quitGameButton = new QuitGameButton(new Coordinate2D(getWidth() / 2, getHeight() * 5/6), "Quit Game");
+    private void addQuitGameButton() {
+        var quitGameButton = new QuitGameButton(new Coordinate2D(getWidth() / 2, getHeight() * 5 / 6), "Quit Game");
         addEntity(quitGameButton);
-
     }
 }
