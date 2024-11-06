3f916d05fb53f14718fc9b4678a40dda8a6448ea
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-11-06 21:35:12 +0100

    refactor

--- src/main/java/AlienDestruction/Game/EnemySpawner.java
+++ src/main/java/AlienDestruction/Game/EnemySpawner.java
@@ -13,37 +13,17 @@ import java.util.Random;
 
 public class EnemySpawner extends EntitySpawner {
 
-    /**
-     * Player player is final:
-     * Dit betekent dat de EntitySpawner-instantie altijd een geldige referentie naar de Player-instantie heeft.
-     * Dit voorkomt dat er fouten optreden door null-waarden of ongeldige verwijzingen.
-     */
     private final Player player;
-    /**
-     * Level level is final:
-     * Dit betekent dat de EntitySpawner-instantie altijd een geldige referentie naar de Level-instantie heeft.
-     * Dit voorkomt dat er fouten optreden door null-waarden of ongeldige verwijzingen.
-     */
+
     private final Level level;
 
-    /**
-     * GameScreen gameScreen is final:
-     * Dit betekent dat de EntitySpawner-instantie altijd een geldige referentie naar de gameScreen-instantie heeft.
-     * Dit voorkomt dat er fouten optreden door null-waarden of ongeldige verwijzingen.
-     */
     private final GameScreen gameScreen;
     private int enemyTypeIndex = 0;
 
     int[][] levelData;
     int difficulty;
 
-    /**
-     * EnemySpawner(Player player, Level level, GameScreen gameScreen)`: Deze constructor initialiseert de spawner met een referentie naar de speler, de leveldata en het gamescherm.
-     *  Stelt de initiële spawnvertraging in op 1200 milliseconden.
-     * @param player Een referentie naar het spelerobject in het spel, omdat deze interageert met Player
-     * @param level Een referentie naar het Level-object met de leveldata.
-     * @param gameScreen Een referentie naar het GameScreen-object waar de entiteiten worden gespawned.
-     */
+
     public EnemySpawner(Player player, Level level, GameScreen gameScreen, App app) {
         super(1200);
         this.level = level;
@@ -58,54 +38,39 @@ public class EnemySpawner extends EntitySpawner {
             }
         }
     }
-/**
-    protected void spawnEntities()`:
- *      Deze methode wordt periodiek aangeroepen om vijanden en obstakels te spawnen op basis van de huidige levelgegevens.
- *      Haalt het huidige levelnummer en de leveldata op uit het `level`-object.
- *      Itereert door de vijandtypen gedefinieerd voor het huidige level met behulp van `enemyTypeIndex`.
- *      Roept `spawnEnemyFromLevel(enemyType)` aan om de juiste vijand of obstakel te spawnen op basis van het vijandtype.
- *      Verhoogt `enemyTypeIndex` om naar het volgende vijandtype in de leveldata te wijzen
-     *  Als alle vijandtypen in het huidige level zijn gespawned:
-     *  Reset `enemyTypeIndex` naar 0.
-     *  Verhoogt het huidige levelnummer en het spelerlevelnummer in het `level`-object.
-     *  Werk de leveltekst bij die op het scherm wordt weergegeven.
-     *  Als het einde van alle levels is bereikt, wordt het huidige levelnummer opnieuw ingesteld op 1.
- *      */
+
     @Override
     protected void spawnEntities() {
-
         int indexLevelNumber = level.getIndexLevelNumber();
         int[] currentLevel = levelData[indexLevelNumber - 1];
         spawnEnemyFromLevel(currentLevel[enemyTypeIndex]);
         enemyTypeIndex++;
 
-        // level up
         if (enemyTypeIndex >= currentLevel.length) {
-            enemyTypeIndex = 0;
-            level.advanceLevel();
-            updateLevelText();
-            gameScreen.pause();
+            levelUp();
         }
 
-        // continuously increase difficulty
         if (indexLevelNumber >= levelData.length) {
-            level.setIndexLevelNumber(1);
-            for (int lvl = 0; lvl < levelData.length; lvl++) {
-                for (int enemy = levelData[lvl].length - 1; enemy >= 0; enemy--) {
-                    levelData[lvl][enemy]++;
-                }
+            resetAndIncreaseDifficulty();
+        }
+    }
+
+    private void levelUp() {
+        enemyTypeIndex = 0;
+        level.advanceLevel();
+        updateLevelText();
+        gameScreen.pause();
+    }
+
+    private void resetAndIncreaseDifficulty() {
+        level.setIndexLevelNumber(1);
+        for (int[] level : levelData) {
+            for (int enemy = level.length - 1; enemy >= 0; enemy--) {
+                level[enemy]++;
             }
         }
     }
 
-    /**
-     *  Deze methode krijgt een vijandtype als invoer en spawnt de corresponderende vijand of obstakel.
-     *  - Creëert een nieuw `GameEntity`-object op basis van het vijandtype.
-     *  - Stelt de beginpositie van de vijand/obstakel in met behulp van een willekeurige X-coördinaat en een start-Y-coördinaat boven het scherm.
-     *  - Roept de methode `spawn(e)` aan om de vijand/obstakel aan het spel toe te voegen.
-     *  - `public void updateLevelText()`:
-     * @param enemyType is de invoer van het type vijand.
-     */
     public void spawnEnemyFromLevel(int enemyType){
         GameEntities e;
         int randomX = Helper.getRandomX(gameScreen.getWidth());
@@ -135,18 +100,12 @@ public class EnemySpawner extends EntitySpawner {
         }
     }
 
-    /**
-     * Deze methode updatet de leveltekst die op het scherm wordt weergegeven om het huidige spelerlevelnummer weer te geven.
-     */
+
     public void updateLevelText(){
         TextEntity levelText;
         gameScreen.getLevelText().setText("Level: " + level.getPlayerLevelNumber());
     }
 
-    /**
-     * Deze methode berekent een snelheidstoename op basis van het huidige spelerlevelnummer.
-     * @return Retourneert een waarde die gelijk is aan het spelerlevelnummer vermenigvuldigd met 0.1.
-     */
     public double getSpeedIncrease() {
         double increase = level.getPlayerLevelNumber() * 0.1;
         return increase;
