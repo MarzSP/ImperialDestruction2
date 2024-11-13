0f0fc03b02407cb2a89b198fe210c6b2c38b5ce0
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-11-13 14:01:03 +0100

    refactor booster

--- src/main/java/AlienDestruction/Game/Booster.java
+++ src/main/java/AlienDestruction/Game/Booster.java
@@ -6,19 +6,6 @@ import javafx.scene.input.KeyCode;
 import java.util.List;
 import java.util.Set;
 
-/**
- * Booster is een implementatie van TimerContainer:
- *  de "Booster" knop [W] op het toetsenbord, activeert in Game Screen een tijdelijke boost voor de speler met een cooldown periode.
- *  * Implements: TimerContainer van Yaeger
-
- * Booster:
- * BoostTime: Statische final variabele met de duur van de boost in seconden (10). Deze is statisch final omdat deze niet zal veranderen.
- * CoolDownTime: Statische final variabele met de duur van de cooldown periode in seconden (20).
- * boosterActive: Boolean is eem variabele die aangeeft of de boost actief is (true) of niet actief (false).
- * coolingDownActive: Boolean variabele die aangeeft of de cooldown periode actief is (true) of niet (false).
- * timer: `SelfTimer` object dat gebruikt wordt om de boost en cooldown te timen.
-
- */
 public class Booster implements TimerContainer {
     static final int BoostTime = 5;
     static final int CoolDownTime = 10;
@@ -27,10 +14,6 @@ public class Booster implements TimerContainer {
     protected boolean coolingDownActive = false;
     public SelfTimer timer = new SelfTimer(0);
 
-    /**
-     * isActive():
-     * @return Retourneert `true` als de boost actief is, anders `false`.
-     */
     public boolean isActive() {
         return this.boosterActive;
     }
@@ -53,19 +36,12 @@ public class Booster implements TimerContainer {
         return coolingDownActive;
     }
 
-    /**
-     * setupTimers(): Pauzeert de interne timer en voegt deze toe aan de lijst met timers.
-     */
     @Override
     public void setupTimers() {
         this.timer.pause();
         addTimer(this.timer);
     }
 
- /** onMouseButtonPressed(MouseButton button, Coordinate2D coordinate2D):
-            *  Controleert of de boost of cooldown actief is, zo ja, dan gebeurt er niks.
-            *   Activeert de boost - start de timer voor de boost tijd en hervat de timer.
- **/
     public void onPressedKeysChange(Set<KeyCode> pressedKeys){
         if (this.boosterActive || this.coolingDownActive) {
            return;
@@ -84,11 +60,6 @@ public class Booster implements TimerContainer {
     public List<Timer> getTimers() {
         return null;
     }
-
-    /**
-    Booster Innerlijke klasse:
-     *  SelfTimer: Extends de `Timer` klasse en handelt de boost en cooldown functies af.
-    **/
 
     public class SelfTimer extends Timer {
         protected SelfTimer(long intervalInMs) {
