dc4d1ce42f17fceb2162ee5016953a0b35b7b9cd
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-11-13 13:59:40 +0100

    refactor highscore

--- src/main/java/AlienDestruction/Game/Highscore.java
+++ src/main/java/AlienDestruction/Game/Highscore.java
@@ -2,27 +2,15 @@ package AlienDestruction.Game;
 
 import AlienDestruction.Entities.Player;
 
-/**De klasse Highscore houdt bij van Player: de hoogste score die de speler heeft behaald (highscore).
- * Daarnaast kan de klasse de huidige score van de speler (currentScore) bijhouden.
- *
- */
 public class Highscore {
     private Player player;
     private int highscore = 0;
     private int currentScore;
 
-    /**
-     * Constructor Highscore initialiseert een highscore object, en krijgt Player als argument.
-     * @param player
-     */
     public Highscore(Player player){
         this.player = player;
     }
 
-    /**
-     * De methode updateCurrentScore: is een attribuut van de class met de huidige score van de speler.
-     * Player class heeft een getScore() methode die de huidige score van de speler retourneert.
-     */
     public void updateCurrentScore(){
        currentScore = player.getScore();
         }
